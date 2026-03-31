bfc8ab39a689899a13305087c3e7e324f90f951d
Roman <romchikus1987@gmail.com>
2026-03-31 11:34:38 +0300

    refactor: change existing route for expeses category, add new routes

--- server/src/routes/expenseRoutes.ts
+++ server/src/routes/expenseRoutes.ts
@@ -1,9 +1,21 @@
 import { Router } from 'express'
-import { getExpensesByCategory } from '../controllers/expenses/expenseController'
+import {
+  getExpensesByCategory,
+  getExpenses,
+  createExpenseRecord,
+  updateExpenseRecord,
+  deleteExpenseRecord,
+} from '../controllers/expenses/expenseController'
 import { asyncHandler } from '../middleware/asyncHandler'
 
 const router = Router()
 
-router.get('/', asyncHandler(getExpensesByCategory))
+router.get('/by-category', asyncHandler(getExpensesByCategory))
+
+//CRUD endpoints for the Витрати page
+router.get('/', asyncHandler(getExpenses))
+router.post('/', asyncHandler(createExpenseRecord))
+router.patch('/:expenseId', asyncHandler(updateExpenseRecord))
+router.delete('/:expenseId', asyncHandler(deleteExpenseRecord))
 
 export default router
